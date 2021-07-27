62b23afb98607baf1963c3da0885f94aeddcf8a7
BrokenDraft <42341867+BrokenDraft@users.noreply.github.com>
2021-07-27 15:55:50 +0200

    pulling updates
    zsh and git aren't found if the sources aren't up to date (especially on fresh installs)

--- good_boi.sh
+++ good_boi.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sudo apt update && sudo apt upgrade
 sudo apt install zsh
 sudo apt install git # ironic isn't it ?
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
